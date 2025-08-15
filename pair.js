583697500dc51e4ec6c95c56b6afafbc3348f014
1thilina <148460963+1thilina@users.noreply.github.com>
2025-08-15 07:15:42 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -86,7 +86,7 @@ router.get('/', async (req, res) => {
                 }
             });
         } catch (err) {
-            exec('pm2 restart danuwa-md');
+            exec('pm2 restart thila-md');
             console.log("service restarted");
             ThilaPair();
             await removeFile('./session');
@@ -100,7 +100,7 @@ router.get('/', async (req, res) => {
 
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ' + err);
-    exec('pm2 restart danuwa');
+    exec('pm2 restart thila');
 });
 
 
